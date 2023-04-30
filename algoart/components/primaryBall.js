d67f5e920884109f0ba4eb53b9d27fe9f15023df
KintsugiCode <liam.going@code.berlin>
2023-04-30 15:23:46 +0200

    [update] movement updated

--- algoart/components/primaryBall.js
+++ algoart/components/primaryBall.js
@@ -1,34 +1,17 @@
-
 class PrimaryBall {
-  constructor(x, y, d, xSpeed, ySpeed) {
+  constructor(x, y, d, zIndex, chaosOrbit, xSpeed, ySpeed) {
     this.x = x;
     this.y = y;
     this.d = d;
+    this.zIndex = zIndex;
+    this.chaosOrbit = chaosOrbit;
     this.xSpeed = xSpeed;
     this.ySpeed = ySpeed;
   }
 
-  move() {
-    randomDestabilizerX = random(randomDestabilizerArray);
-    randomDestabilizerY = random(randomDestabilizerArray);
-
-    // x values of movement with destabilizer
-    this.x += this.xSpeed * randomDestabilizerX * sin(this.y);
-    // boundary behaviour
-    if (this.x - this.d / 2 < 0 || this.x + this.d / 2 > windowWidth) {
-      this.xSpeed *= -1;
-    }
-    
-    // y values of movement with destabilizer
-    this.y += this.ySpeed * randomDestabilizerY * sin(this.x);
-    // boundary behaviour
-    if (this.y - this.d / 2 < 0 || this.y + this.d / 2 > windowHeight) {
-      this.ySpeed *= -1;
-    }
-  }
 
   show() {
-    circle(this.x, this.y, this.d);
-    
+ 
+    sphere(this.d / 4);
   }
 }
--- algoart/sketch.js
+++ algoart/sketch.js
@@ -7,27 +7,29 @@ let errorM = false;
 let x = 0;
 let asteroids = {};
 let currentDate = "";
+let zIndex = [];
+let chaosOrbit = 0;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  translate(-windowWidth * 2, windowHeight * 2);
   background(58, 107, 53);
-  
-  stroke(58, 107, 53);
-  
+
+  noFill();
+
   getAsteroids();
   setInterval(getAsteroids, 3600000); // get data every hour
 }
-function getAsteroids(){
+function getAsteroids() {
   currentDate = new Date().toISOString().split("T")[0];
   url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${currentDate}&end_date=${currentDate}&api_key=vnc4nPGb2oXoSGq81OJMmpKDsqpmLWG3nnfSkW1p `;
   loadJSON(url, createAsteroids);
 }
-function createAsteroids(asteroidData){
+function createAsteroids(asteroidData) {
   button1 = createButton("Look up");
   button1.size(80, 30);
   button1.position(windowWidth / 2 - 40, windowHeight / 2 - 15);
@@ -36,28 +38,93 @@ function createAsteroids(asteroidData){
   button1.style("border-radius", "8px");
   button1.mousePressed(function () {
     for (let i = 1; i <= asteroidData.element_count; i++) {
+      zIndex[i - 1] =
+        asteroidData.near_earth_objects[currentDate][i - 1].estimated_diameter.meters.estimated_diameter_max / 2;
+    }
+    for (let i = 1; i <= asteroidData.element_count; i++) {
+      chaosOrbit = random([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
       balls[i - 1] = new PrimaryBall(
-        windowWidth / random(1,4),
-        windowHeight / random(1,4) ,
+        windowWidth / i,
+        windowWidth / i,
         asteroidData.near_earth_objects[currentDate][i - 1].estimated_diameter.meters.estimated_diameter_max / 2,
+        zIndex[i - 1],
+        chaosOrbit,
         0.5,
         0.5
-        );
-      }
-      
-      hideUI();
-    });
+      );
+    }
+    console.log("🚀 ~ file: sketch.js:56 ~ chaosOrbit:", chaosOrbit);
 
-
+    hideUI();
+  });
 }
 
 function draw() {
-  
-  setPrimaryBallGradient(203, 149);
+  background(58, 107, 53);
+  // setPrimaryBallGradient(203, 149);
 
   for (let i = 1; i <= balls.length; i++) {
-    console.log(balls[i - 1]);
-    balls[i - 1].move();
+    if (balls[i - 1].chaosOrbit == 1) {
+      rotateY(millis() / 5000);
+      rotateX(millis() / 5000);
+      translate(150, 30, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 2) {
+      rotateY(millis() / 6000);
+      rotateX(millis() / 6000);
+      translate(-150, -30, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 3) {
+      rotateY(millis() / 7000);
+      rotateX(millis() / 7000);
+      translate(-150, 30, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 4) {
+      rotateY(millis() / 4500);
+      rotateX(millis() / 4500);
+      translate(150, -30, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 5) {
+      rotateY(millis() / 5500);
+      rotateX(millis() / 5500);
+      translate(130, 40, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 6) {
+      rotateY(millis() / 6500);
+      rotateX(millis() / 6500);
+      translate(-130, -40, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 7) {
+      rotateY(millis() / 5000);
+      rotateX(millis() / 5000);
+      translate(-130, 40, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 8) {
+      rotateY(millis() / 6000);
+      rotateX(millis() / 6000);
+      translate(130, -40, 0);
+    }
+    if (balls[i - 1].chaosOrbit == 9) {
+      rotateY(millis() / 7000);
+      rotateX(millis() / 7000);
+      translate(100, 20, 20);
+    }
+    if (balls[i - 1].chaosOrbit == 10) {
+      rotateY(millis() / 4500);
+      rotateX(millis() / 4500);
+      translate(-100, -20, 20);
+    }
+    if (balls[i - 1].chaosOrbit == 11) {
+      rotateY(millis() / 5500);
+      rotateX(millis() / 5500);
+      translate(-100, 20, 20);
+    }
+    if (balls[i - 1].chaosOrbit == 12) {
+      rotateY(millis() / 6500);
+      rotateX(millis() / 6500);
+      translate(100, -20, 20);
+    }
+
     balls[i - 1].show();
   }
 }
@@ -65,4 +132,3 @@ function draw() {
 function hideUI() {
   button1.hide();
 }
-
